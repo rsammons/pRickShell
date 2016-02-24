8b0597a8b6d2510924b958032bdb796945c54ba2
rsammons <rsammons@diligent.com>
2016-02-24 12:43:36 -0500

    Chris voice

---
+++ source/Invoke/_Invoke-ChrisVoice.ps1
@@ -0,0 +1,20 @@
+function Invoke-Chris
+{
+    param(
+        [Parameter(ValueFromPipeline=$true)]
+        [string] $say 
+    )
+    Begin {
+
+    }
+    process
+    {
+        $rate = $voice.Rate
+        $voice.Rate = -10
+        $voice.Speak("$say, chris chris chris, push it, push it real good") | out-null; 
+        $voice.Rate = $rate
+    }
+}
+
+
+Export-ModuleMember -Function "Invoke-Chris"
