4394a7ffd0b3bfff41cae276b2cb0e3ec2fcae72
Rick Sammons <rsammons@boardbooks.com>
2016-03-01 16:51:15 -0500

    more chris

--- source/Invoke/_Invoke-ChrisVoice.ps1
+++ source/Invoke/_Invoke-ChrisVoice.ps1
@@ -2,7 +2,8 @@ function Invoke-Chris
 {
     param(
         [Parameter(ValueFromPipeline=$true)]
-        [string] $say 
+        [string] $say,
+		[string] $key=1
     )
     Begin {
         $voice = New-Object -ComObject SAPI.SPVoice
@@ -11,7 +12,19 @@ function Invoke-Chris
     {
         $rate = $voice.Rate
         $voice.Rate = -10
-        $voice.Speak("$say, chris chris chris, push it, push it real good") | out-null; 
+		$voices = $voice.GetVoices('',''); 		
+		$cnt = 0
+		$voices | % { 
+			$cnt = $cnt + 1;
+			if ($cnt = $key) {
+			#if ($_.Id -eq "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-GB_HAZEL_11.0") {
+				$voice.Voice = $_ 
+			}
+		}
+		#$voice.GetVoices('','') | where $true.Id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-GB_HAZEL_11.0"
+        #$voice.Speak("$say, chris chris chris, push it, push it real good") | out-null; 
+		$voice.Speak("$say, chris") | out-null; 
+		
         $voice.Rate = $rate
     }
 }
