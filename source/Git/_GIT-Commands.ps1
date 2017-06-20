47a2ec8ac107a2d5be3ec1f89c3fcbe42eaef9b1
Rick Sammons <rick@sammons.net>
2017-06-20 14:46:19 -0400

    git stuffs

--- source/Git/_GIT-Commands.ps1
+++ source/Git/_GIT-Commands.ps1
@@ -29,4 +29,45 @@ function RS-Gitall {
     }
 }
 
+function RS-GitMerge {
+    Param (
+        [Parameter(ValueFromPipeline=$true)]
+        
+		[string] $FromBrach = "dev"
+    )
+    Begin {        
+        Install-Module Posh-Git -Force
+		[System.Collections.ArrayList] $localbranches = RS-PullAllBranches
+    }
+    Process {
+
+		$localbranches | % {
+			Write-Host $_
+			git checkout $_ | Out-Null
+			Start-Sleep -s 5
+			git merge $FromBrach 
+			
+		}
+    }
+}
+
+function RS-PullAllBranches() {
+	$branches = git branch
+	[System.Collections.ArrayList] $localbranches = @()
+	foreach($branch in $branches){
+		$fixedBranch = $branch.Substring(2, $branch.Length - 2)
+		$trackedExpression = "branch." + $fixedBranch + ".merge"
+		$trackedBranch = git config --get $trackedExpression
+		#  Write-Host($trackedBranch)
+		if([string]::IsNullOrEmpty($trackedBranch))
+		{
+			[void]$localbranches.Add($fixedBranch)
+		}	
+	}
+
+	return $localbranches
+}
+
 Export-ModuleMember -Function "RS-Gitall"
+Export-ModuleMember -Function "RS-GitMerge"
+Export-ModuleMember -Function "RS-PullAllBranches"
